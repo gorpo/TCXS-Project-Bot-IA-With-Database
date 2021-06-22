546b4d3e515818a3bf5b8fb23dc7bde27343be34
gorpoorko <68359752+gorpoorko@users.noreply.github.com>
2021-06-22 16:09:31 -0300

    Update config.py

--- config.py
+++ config.py
@@ -18,17 +18,17 @@ import os
 
 
 #LOCAL CONFIG rodar em local host use as linhas abaixo---> "893192395:AAGUm_2rdfwz9Uxc_O7H8WneEzup3XSfNYU"
-token = "893192395:AAGUm_2rdfwz9Uxc_O7H8WneEzup3XSfNYU"
-# marcinho'1096480409:AAE6sg6eJZtH5Z_TEIzgq10SQtCvGf4KYSc' #TOken do
-# luppy"893192395:AAGUm_2rdfwz9Uxc_O7H8WneEzup3XSfNYU"
+token = "893192395:AAGUm_2rdfwz9Uxsdfsdfsdfsfsdfsdf"
+# marcinho'1096480409:AAE6sg6eJZtH5Zeeeewer_TEIzgq10SQtCvGf4KYSc' #TOken do
+# luppy"893192395:AAGUm_2rdfwz9Uxc_O7H8Wnwerg44eEzup3XSfNYU"
 
-token_dropbox = 's4rxVFP2mcAAAAAAAAC2eXinDL33K0tSIhnR1chqOmrtBPy-Dl6Ll0znnRP3Phm5'
+token_dropbox = 's4rxVFP2mcAAAAAAAAC2eXinDL33K444sdfx0tSIhnR1csdfsdfsdfm5'
 
 
 #permissoes
-logs = -1001215401730    #-1001215401730
-sudoers = [522510051,]
-administradores = [522510051,]
+logs = -1001215sdfsdf401730    #-100121540173sdfsdfsdf0
+sudoers = [522510sdfsdf051,]
+administradores = [52251sdfsdf0051,]
 
 
 
@@ -47,12 +47,12 @@ bot_username = me['username']
 bot_id = me['id']
 keys = dict(
     here = {'app_id': 'key_id_here', 'app_code': 'key_code_here'},  # https://here.com
-    yandex = 'trnsl.1.1.20190811T184100Z.f3e1e6d6d3507525.7ea9c786af32b18cedeb125ca46cc2d9ee154e09',#https://tech.yandex.com/translate
-    giphy = '7f6ws7EvslO9BuaAKie9BieyYnD3OkkT',# https://developers.giphy.com
+    yandex = 'trnsl.1.1.20190811T184100Z.f3e1e6d6d350752sdfsdfsdf5.7ea9c786af32b18cedeb125ca46cc2d9ee154e09',#https://tech.yandex.com/translate
+    giphy = '7f6ws7EvslO9BuaAKsdfsdfsdfie9BieyYnD3OkkT',# https://developers.giphy.com
 )
 
 
-backups_chat = 522510051  # Put a 0, False or None to disable ou seu id privado ou id privado de um canal ou grupo
+backups_chat = 52251eeeeee0051  # Put a 0, False or None to disable ou seu id privado ou id privado de um canal ou grupo
 backup_hours = ['12:45']
 
 git_repo = ('https://github.com/gorpoorko/Manicomio-Bot-IA', 'master') #repositorio para upgrade do bot
